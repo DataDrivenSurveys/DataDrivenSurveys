158a324a7c967a664cf34847b782fa042b0a5a75
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-09-16 17:26:45 +0200

    Update to generate server config files dynamically during deployment

--- volumes/certbot/certbot-entrypoint.sh
+++ volumes/certbot/certbot-entrypoint.sh
@@ -1,30 +1,29 @@
 #!/bin/sh
 
 # Path to the live certificate
-live_cert="/etc/letsencrypt/live/www.datadrivensurvey.com/fullchain.pem"
+live_cert="/etc/letsencrypt/live/${DDS_WEBSITE_URL}/fullchain.pem"
 
 cert_obtained=true
 
-if [ ! -f $live_cert ]; then
-    echo "Certificate does not exist, attempting to obtain a new one..."
-    certbot certonly --webroot -w /var/www/letsencrypt --agree-tos --email no-reply@datadrivensurvey.com -d www.datadrivensurvey.com
-    if [ $? -eq 0 ]; then
-        cert_obtained=true
-    else
-        cert_obtained=false
-    fi
+if [ ! -f "$live_cert" ]; then
+  echo "Certificate does not exist, attempting to obtain a new one..."
+  certbot certonly --webroot -w /var/www/letsencrypt --agree-tos --email "${DDS_EMAIL}" -d "${DDS_WEBSITE_URL}"
+  if [ $? -eq 0 ]; then
+    cert_obtained=true
+  else
+    cert_obtained=false
+  fi
 fi
 
 if [ "$cert_obtained" = true ]; then
-    while :;
-    do
-        # If the certificate does exist, attempt to renew it
-        echo "Certificate exists, checking for renewal..."
-        certbot renew --webroot -w /var/www/letsencrypt
+  while :; do
+    # If the certificate does exist, attempt to renew it
+    echo "Certificate exists, checking for renewal..."
+    certbot renew --webroot -w /var/www/letsencrypt
 
-        # Sleep for 24h hours
-        sleep 24h
-    done
+    # Sleep for 24h hours
+    sleep 24h
+  done
 else
-    echo "Failed to obtain a certificate, stopping..."
+  echo "Failed to obtain a certificate, stopping..."
 fi
--- volumes/nginx/nginx-entrypoint.sh
+++ volumes/nginx/nginx-entrypoint.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 
+export DOLLAR='$'
+. /.env.deploy.local
+export DDS_WEBSITE_URL="${DDS_WEBSITE_URL}"
+
+envsubst </etc/nginx/nginx.conf.template >/etc/nginx/nginx.conf
+# && nginx -g 'daemon off;'
+
 # Start NGINX
 echo "Starting NGINX..."
 nginx
 
 # Check if NGINX is running
 if [ $? -ne 0 ]; then
-    echo "NGINX failed to start!"
-    exit 1
+  echo "NGINX failed to start!"
+  exit 1
 fi
 
 echo "NGINX started!"
 
 # Loop indefinitely
-while :; 
-do 
-    # Sleep for a day
-    sleep 1d
-    
-    echo "Reloading NGINX..."
-    # Restart the service to reload the configuration and certificates
-    nginx -s reload
+while :; do
+  # Sleep for a day
+  sleep 1d
+
+  echo "Reloading NGINX..."
+  # Restart the service to reload the configuration and certificates
+  nginx -s reload
 done
