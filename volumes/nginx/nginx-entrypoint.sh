de7aa64efab63354879a114cb6ad029225671fba
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-09-17 16:54:36 +0200

    Update nginx template and compose files

--- volumes/nginx/nginx-entrypoint.sh
+++ volumes/nginx/nginx-entrypoint.sh
@@ -3,6 +3,15 @@
 export DOLLAR='$'
 . /.env.deploy.local
 export DDS_WEBSITE_URL="${DDS_WEBSITE_URL}"
+export BACKEND_SERVICE_NAME="${BACKEND_SERVICE_NAME}"
+
+if [ "$SELF_SIGNED_SSL" = "true" ]; then
+  export SSL_CERTIFICATE="/etc/self-signed-ssl/fullchain.pem"
+  export SSL_CERTIFICATE_KEY="/etc/self-signed-ssl/privkey.pem"
+else
+  export SSL_CERTIFICATE="/etc/letsencrypt/live/${DDS_WEBSITE_URL}-0002/fullchain.pem"
+  export SSL_CERTIFICATE_KEY="/etc/letsencrypt/live/${DDS_WEBSITE_URL}-0002/privkey.pem"
+fi
 
 envsubst </etc/nginx/nginx.conf.template >/etc/nginx/nginx.conf
 # && nginx -g 'daemon off;'
